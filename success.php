e96999e8c96fea289fe5c56d424b584a01db08d1
ခွန်ဖန်ဒွဲ့ <127810188+KhunPhanDuae@users.noreply.github.com>
2023-10-02 20:46:24 +0630

    Create success.php

---
+++ success.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Success</title>
+</head>
+<body>
+    <h1>Success</h1>
+    <p>Your submission was successful!</p>
+    <p><a href="index.php">Back to Home</a></p>
+</body>
+</html>
