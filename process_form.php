4c6f47f00573eafb5d2eef9c8745b32b92cc885e
ခွန်ဖန်ဒွဲ့ <127810188+KhunPhanDuae@users.noreply.github.com>
2023-10-02 19:39:51 +0630

    Create process_form.php

---
+++ process_form.php
@@ -0,0 +1,7 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    $postContent = $_POST["post-content"];
+    // Save $postContent to a database or file
+}
+// Redirect back to the page or show a confirmation message
+?>
